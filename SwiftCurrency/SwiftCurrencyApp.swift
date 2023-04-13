225896a42a913485f1cceee77eb11dc69fadbcdf
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-04-13 09:31:34 +0200

    Initial Commit

---
+++ SwiftCurrency/SwiftCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCurrencyApp.swift
+//  SwiftCurrency
+//
+//  Created by Tobias Sörensson on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
